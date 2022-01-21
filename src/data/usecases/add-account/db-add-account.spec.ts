e2269092d8d1ae6bd3b68a3f9ae7766c797a7971
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-01-20 21:15:15 -0300

    test: ensure DbAddAccount throw if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -93,4 +93,20 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
+
+  test('Should throw if Encrypter throws', async () => {
+    const { SUT, addAccountRepositoryStub } = makeSUT()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const promise = SUT.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
