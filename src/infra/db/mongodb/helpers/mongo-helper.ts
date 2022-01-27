c2405668755099ba25bfffb8cc2212065ec7a5ea
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-01-27 14:41:26 -0300

    feat: ensure AccountMongoRepository return an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,13 +1,18 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
 
   async connect (uri: string) {
-    this.client = await MongoClient.connect(global.__MONGO_URI__)
+    // @ts-expect-error: Unreachable code error
+    this.client = await MongoClient.connect(process.env.MONGO_URL)
   },
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
